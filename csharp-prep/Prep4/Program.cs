73210feae86a9ac2fc248b1ffdd91e0e894c0db4
marcelo.carmo <marcelo.carmo54@gmail.com>
2024-01-11 10:23:44 -0300

    update

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection.Metadata;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Will sum the amount of numbers
+        int count = 0;
+
+        // creating a list of numbers
+        List<int> numbers = new List<int>();
+
+        for (int number = -1; number != 0; count = count + number)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");  
+            string userInput = Console.ReadLine();
+            
+            // converting string into int
+            number = int.Parse(userInput);
+            // adding itens to the list
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        Console.WriteLine($"The sum is: {count}");
+
+        double average = count / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine();
+
+        Console.WriteLine($"This is the size of the list: {numbers.Count}");
     }
 }
