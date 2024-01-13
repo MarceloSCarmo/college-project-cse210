c5b7cb8e315b8f932a58cf37ddbb3f77e4afaae9
marcelo.carmo <marcelo.carmo54@gmail.com>
2024-01-13 15:35:01 -0300

    update

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,4 +6,29 @@ class Program
     {
         Console.WriteLine("Hello Prep5 World!");
     }
+
+    // Displays the message, "Welcome to the Program!"
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program");
+    }
+
+    // Asks for and returns the user's name (as a string)
+    static string PromptUserName(string userName)
+    {
+        Console.WriteLine("What is your name?");
+        userName = Console.ReadLine();
+        return userName;
+    }
+
+    // Asks for and returns the user's favorite number (as an integer)
+    static int PromptUserNumber(string userFavoriteNumber)
+    {
+        Console.WriteLine("What is your name?");
+        userFavoriteNumber = Console.ReadLine();
+        // Converting string to int
+        int FavoriteNumber = int.Parse(userFavoriteNumber);
+
+        return FavoriteNumber;
+    }
 }
