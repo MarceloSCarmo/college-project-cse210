fc9356414171440e6cbfbe978632f19a52aa26f6
marcelo.carmo <marcelo.carmo54@gmail.com>
2024-01-16 00:00:46 -0300

    update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Hello Learning02");
+
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Jr DevOps";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        job1.Display();
     }
+
+
+              
 }
