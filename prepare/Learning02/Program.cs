f1a941e7de448eccabd5b708ac354d60bfe73677
marcelo.carmo <marcelo.carmo54@gmail.com>
2024-01-18 23:47:30 -0300

    update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,15 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02");
-
+        // First job
         Job job1 = new Job();
         job1._company = "Apple";
         job1._jobTitle = "Jr DevOps";
         job1._startYear = 2022;
         job1._endYear = 2023;
 
+        // Second job
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Sr DevOps";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+        
+        Resume myResume = new Resume();
+        myResume._name = "Marcelo";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
         job1.Display();
+        job2.Display();
+        
     }
 
 
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,16 @@
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}\nJobs:");
+        
+        foreach (Job job in _jobs)
+        {
+            string myJob = job._jobTitle;
+            Console.WriteLine($"{myJob}");
+        }
+    }
+}
