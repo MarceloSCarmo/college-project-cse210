5765b22ff2843805a52cb098e14253b5bde67d5f
marcelo.carmo <marcelo.carmo54@gmail.com>
2024-01-22 19:05:46 -0300

    update

---
+++ prove/Develop02/Jornal.cs
@@ -0,0 +1,22 @@
+public class Jornal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(newEntry : Entry)
+    {
+
+    }
+    public void DisplayAll()
+    {
+
+    }
+    public void SaveToFile(File:string)
+    {
+
+    }
+    public void LoadFromFile(File:string)
+    {
+
+    }
+
+}
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,11 @@
+public class PromptGenerator
+{
+    public List<string> _prompts = new List<string>();
+
+    Random GetRandomPrompt = new Random();
+    public string GetRandomPrompt.NewPrompt()
+    {
+        
+    }
+
+}
